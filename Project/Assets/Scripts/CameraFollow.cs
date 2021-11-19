28374b37a395bf07bfce69be9f0e90d0850330df
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2021-11-19 14:42:18 +0100

    Basic camera follow

---
+++ Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float CameraHeightOffset = 6;
+    public float CameraPositionOffset = 1;
+    public GameObject target;
+    
+    void Update()
+    {
+        if (target != null)
+        {
+            var cameraCoordinates = transform.position;
+            var targetCoordinates = target.transform.position;
+            var newCameraCoordinates = Vector3.Lerp(cameraCoordinates, targetCoordinates, 0.01f);
+            transform.position = new Vector3(newCameraCoordinates.x, CameraHeightOffset, newCameraCoordinates.z - CameraPositionOffset);
+        }
+    }
+}
